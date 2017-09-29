74a56955a7969b241e72fa756ac45643f2826cf6
DamnedScholar <stickfigureonfire@gmail.com>
2017-09-28 23:21:08 -0500

    Initial commit

---
+++ spec/no-frills-opener-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NoFrillsOpenerView from '../lib/no-frills-opener-view';
+
+describe('NoFrillsOpenerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
